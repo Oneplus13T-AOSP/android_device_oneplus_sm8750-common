79d5d0dacb268dfe71bd228130e89664e6031cbd
RobertGarciaa <chae0218@naver.com>
2026-05-28 14:21:34 +0900

    sm8750-common: aconfigd Daemon Target Package Build Force Inclusion
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- common.mk
+++ common.mk
@@ -7,6 +7,10 @@
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# aconfigd
+PRODUCT_PACKAGES += \
+    libaconfig_storage_read_api_cc
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
