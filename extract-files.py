f373db143ee31522f2d77707f1400cbdb38e2870
ralph950412 <ralph950412@gmail.com>
2026-06-30 03:14:25 +0000

    sm8750-common: Add libaudiobase dependency to libwfdmmsrc_system AudioSystem and its symbols have been moved to libaudiobase.
    Reference:
    https: //android.googlesource.com/platform/frameworks/av/+/24d4f12702b932e25f9b8585e0e9120cd242ce94
    Change-Id: I718ae5d37bf7a9c2dc74243fd1f63e6cc0ede9d4
    Signed-off-by: n0escape-exe <viaan4542@gmail.com>
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- extract-files.py
+++ extract-files.py
@@ -119,6 +119,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libaudiobase.so')
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .add_needed('libgui_shim.so'),
 }  # fmt: skip
 
