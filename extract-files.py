b9aff584f1d47f6ce14df33febd675e7e9edc776
binbin323 <bin@dhao.cc>
2026-06-30 03:12:27 +0000

    sm8750-common: Patch libwfdmmsrc_system with libgui_shim
    Change-Id: Ieac7d379639baf1127a469a75743ecd723d16d33
    Signed-off-by: n0escape-exe <viaan4542@gmail.com>
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- extract-files.py
+++ extract-files.py
@@ -118,6 +118,8 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .add_needed('libaudiobase.so')
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
